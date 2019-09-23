7e8338ae0637b2b01cfc29a0ba7961f71d7bd313
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-09-23 18:44:36 +0700

    Add files via upload

---
+++ src/if2/pkg10118067/latihan15/IF210118067Latihan15.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118067.latihan15;
+
+/**
+ *
+ * @author
+ * Nama     : Agus Awaludin
+ * Kelas    : IF-2
+ * NIM      : 10118067
+ * Latihan 15. Logika
+ */
+public class IF210118067Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("! (a && b) = " + !(a && b));
+    }
+    
+}
